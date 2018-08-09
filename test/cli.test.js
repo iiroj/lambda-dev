59905c764c89f6faa9c25b3beebe66fe4fd6a262
Iiro Jäppinen <iiro@jappinen.fi>
2018-08-09 13:08:05 +0300

    Rename index.js to cli.js

--- test/cli.test.js
+++ test/cli.test.js
@@ -3,7 +3,7 @@ const CliTest = require('command-line-test');
 describe('cli', () => {
   test('Without arguments', async () => {
     const cli = new CliTest();
-    const { error, stdout, stderr } = await cli.exec('node .');
+    const { error, stdout, stderr } = await cli.exec('node cli.js');
 
     expect(error).toMatchSnapshot();
     expect(stdout).toEqual(null);
@@ -12,7 +12,7 @@ describe('cli', () => {
 
   test('Build without entry argument', async () => {
     const cli = new CliTest();
-    const { error, stdout, stderr } = await cli.exec('node . build');
+    const { error, stdout, stderr } = await cli.exec('node cli.js build');
 
     expect(error).toMatchSnapshot();
     expect(stdout).toEqual(null);
@@ -21,7 +21,7 @@ describe('cli', () => {
 
   test('Build without target argument', async () => {
     const cli = new CliTest();
-    const { error, stdout, stderr } = await cli.exec('node . build test/fixtures');
+    const { error, stdout, stderr } = await cli.exec('node cli.js build test/fixtures');
 
     expect(error).toMatchSnapshot();
     expect(stdout).toEqual(null);
@@ -30,7 +30,7 @@ describe('cli', () => {
 
   test('Serve without entry argument', async () => {
     const cli = new CliTest();
-    const { error, stdout, stderr } = await cli.exec('node . serve');
+    const { error, stdout, stderr } = await cli.exec('node cli.js serve');
 
     expect(error).toMatchSnapshot();
     expect(stdout).toEqual(null);
