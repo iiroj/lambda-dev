10e6a819b9250aa1c99efe258646f812f5168da1
Iiro Jäppinen <iiro@jappinen.fi>
2018-08-20 07:00:20 +0300

    test: Add jest config

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  collectCoverageFrom: ['lib/**/*.{js}'],
+  collectCoverage: true,
+  coverageReporters: process.env.CI ? ['text-summary'] : ['lcov'],
+  coverageThreshold: {
+    global: {
+      statements: 1,
+      branches: 1,
+      functions: 1,
+      lines: 1
+    }
+  },
+  testEnvironment: 'node',
+  testRegex: 'test/.*\\.test\\.js$'
+};
